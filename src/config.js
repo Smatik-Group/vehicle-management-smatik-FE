2e0a6d5215cdb04b65cc46dc631bff284a5a723d
softmindsol <softmindsol@gmail.com>
2025-02-13 15:38:36 +0500

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = "https://vehicle-management-smatik-be.vercel.app";
+export const BASE_URL = "https://vehicle-management-smatik-be-rho.vercel.app";
 // export const BASE_URL = "http://localhost:8084";
